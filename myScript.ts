aeb5f21b3766e4255f162424eb4c0a6ebbea22d5
jzahrndt11 <jerzahrndt@gmail.com>
2024-02-04 00:08:46 -0600

    Done with Primitive types

---
+++ myScript.ts
@@ -0,0 +1,39 @@
+//! Primitive Types--------------------------------------------------------------------------------------------
+/*  
+    *  boolean
+    *  string
+    *  number
+*/ 
+
+    const myString: string = 'Hello World'
+    const myNum: number = 5
+    const isBool: boolean = true
+
+    //~ Set both parameters and return type to number
+    const add = (num1: number, num2: number): number => {
+        return num1 + num2
+    }
+
+    console.log(add(1, 2))
+
+
+
+
+
+
+
+
+//* Typescript Documentation
+
+    /*
+        ~Every Day Types (90% of whats covered): 
+            - https://www.typescriptlang.org/docs/handbook/2/everyday-types.htmlLinks to an external site.
+
+        ~Enums:
+            - https://www.typescriptlang.org/docs/handbook/enums.htmlLinks to an external site.
+
+        ~Generics (There is a lot more to generics than what I covered. But what I did cover covers pretty much all use cases for the class)
+            - https://www.typescriptlang.org/docs/handbook/2/generics.htmlLinks to an external site.
+    */
+
+
